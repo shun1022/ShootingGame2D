3fa0b2a697e4a25e666e5546b5d9dbe16ab9d98b
shun <shun@fuji.local>
2018-02-18 23:34:52 +0900

    自機と敵機で共通する部分をSpaceship.csに実装

---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class SpaceShip : MonoBehaviour {
+
+    public float Speed;
+    public float ShotDelay;
+    public GameObject Bullet;
+
+    public void Shot (Transform origin)
+    {
+        Instantiate(Bullet, origin.position, origin.rotation);
+    }
+
+    public void Move (Vector2 direction)
+    {
+        GetComponent<Rigidbody2D>().velocity = direction * Speed;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
