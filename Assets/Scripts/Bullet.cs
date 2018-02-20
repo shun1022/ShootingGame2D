1e9289c837974fab14146b53a431f49da58b614d
shun <shun@fuji.local>
2018-02-20 23:23:53 +0900

    Bullet.csにコメント付加

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour {
 
-    public int speed = 1;
+    // メンバ変数
+    public int Speed = 10;
 
 	// Use this for initialization
 	void Start () {
-        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+        // Rigidbody2Dを取得、速度Speedで移動する
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * Speed;
 	}
 	
 }
