1aec0a011c70c5186d2adc21cd6b3e84b6aa30e3
shun <shun@fuji.local>
2018-02-25 01:53:45 +0900

    弾が一定範囲を出ると消える機能の実装

---
+++ Assets/Scripts/DestroyArea.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyArea : MonoBehaviour {
+
+    // このコライダーに接触してから離れたときに呼びだされるメソッド
+    private void OnTriggerExit2D(Collider2D c)
+    {
+        // 弾を削除する
+        Destroy(c.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
