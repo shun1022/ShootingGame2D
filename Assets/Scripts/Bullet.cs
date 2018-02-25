b252279cfeb80208c505c36a99ec6c151a61dc12
shun <shun@fuji.local>
2018-02-25 21:56:46 +0900

    弾が数秒後自動で消える

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,10 +7,16 @@ public class Bullet : MonoBehaviour {
     // メンバ変数
     public int Speed = 10;
 
+    // メンバ変数
+    public float LifeTime = 5;
+
 	// 最初に一度だけ呼び出されるメソッドStart
 	void Start () {
         // Rigidbody2Dを取得、速度Speedで移動する
         GetComponent<Rigidbody2D>().velocity = transform.up.normalized * Speed;
+
+        // LifeTime杪後に削除
+        Destroy(gameObject, LifeTime);
 	}
 	
 }
