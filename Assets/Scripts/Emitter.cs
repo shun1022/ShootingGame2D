b020cbe19d371e220b51f22992586722d364df00
shun <shun@fuji.local>
2018-02-28 01:34:26 +0900

    Emitter.cs途中まで記述

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour {
+
+    // Waveプレハブを格納するメンバ変数
+    public GameObject[] Waves;
+
+    // 現在のWaveを参照する為のローカル変数
+    private int CurrentWave;
+
+    IEnumerator Start ()
+    {
+        // 
+        if (Waves.Length == 0)
+        {
+            yield break;
+        }
+
+        // 
+        while (true)
+        {
+            // 
+            GameObject Wave = (GameObject)Instantiate(Waves[CurrentWave], transform.position, Quaternion.identity);
+
+
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
