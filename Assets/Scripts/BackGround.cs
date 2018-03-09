580a58b07d5643e7fb80805318c8b01fa252256c
shun <shun@fuji.local>
2018-03-09 14:59:05 +0900

    背景を画面サイズに合わせる

--- Assets/Scripts/BackGround.cs
+++ Assets/Scripts/BackGround.cs
@@ -9,7 +9,15 @@ public class BackGround : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
+        // 画面右上のワールド座標をビューポートから取得
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+
+        // スケールを求める
+        Vector2 scale = max * 2;
+
+        // スケールを変更
+        transform.localScale = scale;
 	}
 	
 	// Update is called once per frame
