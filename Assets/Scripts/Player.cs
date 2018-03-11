c7cdc3e57520a07dbe39d09e69487f7390dad475
shun <shun@fuji.local>
2018-03-11 19:49:08 +0900

    エラー消えたが謎の動きしかしない

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,43 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     // SpaceShipコンポーネントを追加
     SpaceShip SpaceShip;
 
 
-        
+
     // コルーチンでStartメソッドを呼び出す
-    IEnumerator Start () {
-        
+    IEnumerator Start()
+    {
+
         // GetComponentでSpaceShipクラスのインスタンスを取得しローカル変数SpaceShipで保持
         SpaceShip = GetComponent<SpaceShip>();
 
         // 発射するたびにShotDelay秒中断し、再開する
-        while (true) {
+        while (true)
+        {
             SpaceShip.Shot(transform);
             yield return new WaitForSeconds(SpaceShip.ShotDelay);
 
             // GetComponentでAudioSourceを取得しPlayメソッドで再生する
             GetComponent<AudioSource>().Play();
         }
-	}
+    }
 
 
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Pos = transform.position;
+        Vector3 ScreenPos = Input.mousePosition;
+        ScreenPos.z = 0;
+        Vector3 WorldPos = Camera.main.ScreenToWorldPoint(ScreenPos);
 
-        Vector3 pos = transform.position;
-        Vector3 x = Input.mousePosition;
-        Vector3 y = Input.mousePosition;
 
 
         // 移動する向きを決める
-        Vector2 direction = new Vector2(x, y).normalized;
+        Vector3 Direction = (Pos - WorldPos).normalized;
 
         // 移動範囲を制限する
-        Move(direction);
+        Move(Direction);
 
 
         // GetAxisRawメソッドによりキーボード入力で+1か-1を返す
@@ -47,7 +52,7 @@ public class Player : MonoBehaviour {
 
 
 
-	}
+    }
 
     // メソッドMoveの宣言
     void Move(Vector2 direction)
@@ -81,14 +86,14 @@ public class Player : MonoBehaviour {
         string LayerName = LayerMask.LayerToName(C.gameObject.layer);
 
         // レイヤー名がBullet(Enemy)の場合
-        if ( LayerName == "Bullet(Enemy)")
+        if (LayerName == "Bullet(Enemy)")
         {
             // 弾を削除
             Destroy(C.gameObject);
         }
 
         // レイヤー名がBullet(Enemy),Enemyだった場合
-        if ( LayerName == "Bullet (Enemy)" || LayerName == "Enemy")
+        if (LayerName == "Bullet (Enemy)" || LayerName == "Enemy")
         {
             // 機体が爆発する
             SpaceShip.Explosion();
